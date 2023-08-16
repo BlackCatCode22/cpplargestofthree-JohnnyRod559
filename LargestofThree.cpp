0c255581af764a31f0c62da90812fe100334345e
JohnnyRod559 <141672746+JohnnyRod559@users.noreply.github.com>
2023-08-16 11:43:56 -0700

    UPDATED VERSION

--- LargestofThree.cpp
+++ LargestofThree.cpp
@@ -4,32 +4,38 @@ using namespace std;
 
 int main()
 {
+    //Variable Declaration Section
     int num1 = 0;
     int num2 = 0;
     int num3 = 0;
     int Largest = 0;
+    //Username
     string username = "username";
 
 
     cout << "\n\n Hi Welcome to Largest of three program! \n\n";
-//Get user name
+
     cout << "\n\n Please enter your name: ";
     cin >> username;
 
-    cout << "\n\n Welcome, lets get started " << username;
+    cout << "\n\n Welcome, lets get started " << username << "\n\n";
 
-    cout << "\n\n Enter first number: ";
+    cout << "Enter first number: ";
     cin >> num1;
+    cout << "Thank you for entering " << num1 << "\n\n";
 
     cout << "Enter second number: ";
     cin >> num2;
+    cout << "Thank you for entering " << num2 << "\n\n";
 
     cout << "Enter third number: ";
     cin >> num3;
+    cout << "Thank you for entering " << num3 << "\n\n";
 
 
 if (num1 >= num2 & num1>= num3){
     cout << "The largest number is: " << num1;
+
 } if (num2 >= num1 & num2 >= num3) {
     cout << "The largest number is:" << num2;
     }
