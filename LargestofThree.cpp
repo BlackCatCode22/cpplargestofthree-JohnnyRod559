d2a72a42818d34ae15b56ec71348df1b0240a18a
JohnnyRod559 <141672746+JohnnyRod559@users.noreply.github.com>
2023-08-16 11:15:42 -0700

    8/16/23

---
+++ LargestofThree.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 0;
+    int num2 = 0;
+    int num3 = 0;
+    int Largest = 0;
+    string name1 = "name1";
+
+
+    cout << "\n\n Hi Welcome to Largest of three program! \n\n";
+//Get user name
+    cout << "\n\n Please enter your name: ";
+    cin >> name1;
+
+    cout << "\n\n Welcome, lets get started " << name1;
+
+    cout << "\n\n Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter first number: ";
+    cin >> num2;
+
+    cout << "Enter first number: ";
+    cin >> num3;
+
+
+if (num1 >= num2 & num1>= num3){
+    cout << "The largest number is: " << num1;
+} if (num2 >= num1 & num2 >= num3) {
+    cout << "The largest number is:" << num2;
+    }
+    {if (num3 >= num1 & num3 >= num2)
+        cout << "The largest number is: " << num3;
+    }
+     cout << "\n\n Congrats you got your answer! \n\n";
+
+
+
+
+
+
+
+
+    return 0;
+}
